47d0ec24035bafc786b3598a75e0e999b6ed5bc9
GlavniiLeopold <glavnii_leopold@mail.ru>
2017-05-04 22:56:58 +0300

    Add files via upload

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    public Text WinText;
+    public Text DieText;
+    // gravity constant
+    public float g = 9.8f;
+
+    void FixedUpdate()
+    {
+        // normalize axis
+        var gravity = new Vector3(
+            Input.acceleration.x,
+            Input.acceleration.z,
+            Input.acceleration.y
+        ) * g;
+
+        GetComponent<Rigidbody>().AddForce(gravity, ForceMode.Acceleration);
+    }
+        void OnTriggerEnter(Collider other)
+    {
+        //Сцена один
+        if (other.gameObject.tag == "PickUp")
+        {
+            other.gameObject.SetActive(false);
+            WinText.gameObject.SetActive(true);
+            SceneManager.LoadScene(2);
+        }
+        other.gameObject.SetActive(true);
+        WinText.gameObject.SetActive(false);
+        //Сцена два
+        if (other.gameObject.tag == "PickUp2")
+        {
+            WinText.gameObject.SetActive(true);
+            SceneManager.LoadScene(3);
+        }
+        //////////////////////
+        //Сцена три
+        if (other.gameObject.tag == "PickUp3")
+        {
+            WinText.gameObject.SetActive(true);
+        }
+        /////////////////////
+        //Смерть
+        if (other.gameObject.tag == "Enemy")
+        {
+            DieText.gameObject.SetActive(true);
+            SceneManager.LoadScene(3);
+            
+        }
+
+
+
+
+
+
+    }
+
+}
